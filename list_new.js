4de2cd57cef230c6e00a6c9538ae98adfc384178
Marcel Michelfelder <mmichelfelder@schutzwerk.com>
2018-04-05 09:33:27 +0200

    Added proxies

--- list_new.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Marcel Michelfelder on 28.08.2016.
- */
-module.exports = {list:[
-    "128.199.199.41:3128"
-]}
--- phantom.js
+++ phantom.js
@@ -1,15 +1,17 @@
 const puppeteer = require('puppeteer');
 
 // var proxylist = require('./list_new.js');
-var proxylist = require('./list.js');
+// var proxylist = require('./list.js');
 var viewports = require('./viewports.js');
 var fs = require('fs');
 var ualist = fs.readFileSync('ualist.txt').toString().split("\n");
+var proxylist = fs.readFileSync('proxies.txt').toString().split("\n");
 
 var successful = 0;
 var errors = 0;
 async function sendReq (list){
     let proxy = list.pop();
+    let error = false;
     console.log(proxy);
     const browser = await puppeteer.launch({
         args: [
@@ -23,42 +25,53 @@ async function sendReq (list){
             userAgent: getRandomFromList(ualist),
             viewport: getRandomFromList(viewports.list)
         })
-        await page.goto('http://localhost:8001');
-        await page.screenshot({
-            path: './screens/example-' + Date.now() + '.png'
-        });
+        // await page.goto('http://localhost:8001');
+        // await page.screenshot({
+        //     path: './screens/example-' + Date.now() + '.png'
+        // });
 
+        let cookies = await page.cookies();
+        console.log(cookies);
+
+        await timeout(getRandomInt(1000,3000));
 
         // const votebutton = await page.$(".candidate-tile__vote-icon");
         // const res = await votebutton.evaluate( votebutton => votebutton.click() );
         page.click(".candidate-tile__vote-icon");
 
-        await timeout(1000);
+        await timeout(getRandomInt(1000,2000));
 
-        await page.screenshot({
-            path: './screens/example_clicked-' + Date.now() + '.png'
-        });
+        // await page.screenshot({
+        //     path: './screens/example_clicked-' + Date.now() + '.png'
+        // });
 
 
     } catch (err) {
-        console.error('ERROR:', err.message);
+        // console.error('ERROR:', err.message);
+        error = true;
+
     } finally {
-        console.log('.');
+        console.log(error ? 'x' : 'o');
+        error ? successful++ : errors++;
     }
 
     await browser.close();
 
+    let second = 1000;
+    let minute = second * 60;
+    await timeout(getRandomInt(minute,minute * 20));
+
     if(list.length)
         sendReq(list);
     else {
-        console.log("FINISHED..");
+        console.log("FINISHED:");
+        console.log("SUCCESSFUL: " + successful);
+        console.log("ERRORS: " + errors);
         // sendReq(shuffle(proxylist));
     }
-
-
 }
 
-sendReq(shuffle(proxylist));
+sendReq(shuffleArr(proxylist));
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -66,6 +79,14 @@ function timeout(ms) {
 
 function getRandomFromList(list){
     return list[Math.floor(Math.random()*list.length)];
+}
+
+function shuffleArr(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
 function shuffle(a) {
@@ -78,6 +99,10 @@ function shuffle(a) {
         a[j] = x;
     }
     return a;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // (async () => {
